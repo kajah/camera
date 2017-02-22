d55d410011a91bb0fb0d2ff8076d16e46cabcd86
Katharine Jiang <katharine.jiang9@gmail.com>
2017-02-22 14:25:17 -0800

    set up camera

--- app/src/main/java/com/unifyid/katharine/camera/MainActivity.java
+++ app/src/main/java/com/unifyid/katharine/camera/MainActivity.java
@@ -2,12 +2,26 @@ package com.unifyid.katharine.camera;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button pressMe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pressMe = (Button) findViewById(R.id.pressMe);
+        pressMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
+
+
 }
